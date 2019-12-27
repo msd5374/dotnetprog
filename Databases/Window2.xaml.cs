cb523423fe1f192b1a3cb2321b9c5203851d0604
msd5374 <Rajatdiwate94@gmail.com>
2019-12-27 19:30:24 +0530

    databses file day8

--- Databases/Window2.xaml.cs
+++ Databases/Window2.xaml.cs
@@ -100,5 +100,85 @@ namespace Databases
             }
 
         }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=shubhamm;Integrated Security=true";
+            try
+            {
+                cn.Open();
+
+                SqlCommand cmdUpdate = new SqlCommand();
+                cmdUpdate.Connection = cn;
+                cmdUpdate.CommandType = CommandType.Text;
+                cmdUpdate.CommandText = "update Employees set DeptNo=@DeptNo,Basic=@Basic,Name=@Name where EmpNo=@EmpNo";
+
+                cmdUpdate.Parameters.Add(new SqlParameter { ParameterName = "@EmpNo", SourceColumn = "EmpNo", SourceVersion = DataRowVersion.Original });
+                cmdUpdate.Parameters.Add(new SqlParameter { ParameterName = "@DeptNo", SourceColumn = "DeptNo", SourceVersion = DataRowVersion.Current });
+                cmdUpdate.Parameters.Add(new SqlParameter { ParameterName = "@Basic", SourceColumn = "Basic", SourceVersion = DataRowVersion.Current });
+                cmdUpdate.Parameters.Add(new SqlParameter { ParameterName = "@Name", SourceColumn = "Name", SourceVersion = DataRowVersion.Current });
+
+
+
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = CommandType.Text;
+                cmdInsert.CommandText = "insert into Employees values(@EmpNo, @DeptNo, @Basic, @Name)";
+
+                cmdInsert.Parameters.Add(new SqlParameter { ParameterName = "@EmpNo", SourceColumn = "EmpNo", SourceVersion = DataRowVersion.Current });
+                cmdInsert.Parameters.Add(new SqlParameter { ParameterName = "@DeptNo", SourceColumn = "DeptNo", SourceVersion = DataRowVersion.Current });
+                cmdInsert.Parameters.Add(new SqlParameter { ParameterName = "@Basic", SourceColumn = "Basic", SourceVersion = DataRowVersion.Current });
+                cmdInsert.Parameters.Add(new SqlParameter { ParameterName = "@Name", SourceColumn = "Name", SourceVersion = DataRowVersion.Original });
+
+
+
+                SqlCommand cmdDelete = new SqlCommand();
+                cmdDelete.Connection = cn;
+                cmdDelete.CommandType = CommandType.Text;
+                cmdDelete.CommandText = "delete from Employees where EmpNo=@EmpNo";  //use original whenever there is a where clause
+
+
+                cmdDelete.Parameters.Add(new SqlParameter { ParameterName = "@EmpNo", SourceColumn = "EmpNo", SourceVersion = DataRowVersion.Current });
+                //above will be used for the adding multiple rows
+
+
+                // ds.Tables["Emps"].DefaultView.RowFilter = "EmpNO=" + txtEmpNo.Text;
+
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.UpdateCommand = cmdUpdate;
+                da.InsertCommand = cmdInsert;
+                da.DeleteCommand = cmdDelete;
+
+                da.Update(ds, "Emps");
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            cn.Close();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            //DataView dv = new DataView(ds.Tables["Emps"]);
+            //dv.RowFilter = "DeptNo=" + txtDeptNo.Text;
+            //dgEmps.ItemsSource = dv;
+
+            ds.Tables["Emps"].DefaultView.RowFilter = "DeptNo=" + txtDeptNo.Text;
+            //ds.Tables["Emps"].DefaultView.Sort = "DeptNo";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ds.Tables["Emps"].DefaultView.RowFilter = "";
+
+        }
     }
 }
